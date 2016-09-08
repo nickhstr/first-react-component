c9cf39334f31ffd2f81a248669c2b1666d3f35d7
Nick Hester <nickhstr@gmail.com>
2016-09-07 23:42:31 -0700

    feat: initial commit

---
+++ app/index.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloUser = React.createClass({
+	render: function() {
+		return (
+			<div> Hello {this.props.name}</div>
+		)
+	}
+});
+
+ReactDOM.render(
+	<HelloUser name="Nick" />,
+	document.getElementById('app')
+);
